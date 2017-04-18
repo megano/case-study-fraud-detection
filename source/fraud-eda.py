6ed2535f264b54dc51efcd1d574b0041fbcfa40a
Megan ORorke <megororke+1@gmail.com>
2017-04-18 11:55:11 -0700

    added scope to readme, eda updates, separated model from helpers

--- source/fraud-eda.py
+++ source/fraud-eda.py
@@ -13,3 +13,7 @@ df['fraud'] = df.acct_type.map(lambda x: 1 if ((x == 'fraudster') | (x == 'fraud
 
 gts = Gross sales are the grand total of all sale transactions reported in a period
 '''
+
+# # convert time values to time type
+# df['approx_payout_date'] = df['approx_payout_date'].map(lambda x: datetime.datetime.fromtimestamp(x))
+# del df['ticket_types']
